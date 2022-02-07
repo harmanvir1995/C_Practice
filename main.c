bca86c7b81f8ba897541ce13c70ea8c3a8391be4
Harmanvir Singh <59752492+harmanvir1995@users.noreply.github.com>
2022-02-07 18:46:37 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,52 +1,62 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <AGGREGATE.H>
+
 
 int main()
 {
-   enum company{GOOGLE, FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT};
-   enum company company_1 = XEROX;
-   enum company company_2 = GOOGLE;
-   enum company company_3 = EBAY;
-   printf("%d\n", company_3);
+    //Question1
+    printf("----------------------------------------------------------\n");
+    printf("\t\tOutput for the Question 3\n");
+    printf("----------------------------------------------------------\n");
+    float data[] = {112.2, 202.3, 98.3, 99.5, 87.2, 66.3, 581.15, 31.32, 53.36, 95.36};
+    printf("Minimum number in the array is : %-10f\n", min(data, sizeof(data)/sizeof(data[0])));
+    printf("Maximum number present in an array is : %-10f\n", max(data, sizeof(data)/sizeof(data[0])));
+    float myFloat = 2.1234;
+    float *pf = &myFloat;
+    printf("my pointer: %f\n", *pf);
+    printf("Value printed by using pointer method is %f\n", *maxPointer(data,sizeof(data)/sizeof(data[0])));
+    printf("Sum of the array is given by: %f\n", sum(data, sizeof(data)/sizeof(data[0])));
+    printf("Average of an array is given by: %f\n", avg(data, sizeof(data)/sizeof(data[0])));
+      // Question 2
+    printf("\n\n----------------------------------------------------------\n");
+    printf("\t\tOutput for the Question 4\n");
+    printf("----------------------------------------------------------\n");
+    float (*pmin)(float[], int) = &min;
+    float (*pmax)(float[], int) = &max;
+    float (*psum)(float[], int) = &sum;
+    float (*pavg)(float[], int) = &avg;
+    float (*ppseudo_avg)(float[], int) = &pseudo_avg;
+    float (*aggregates[])(float[], int) = {pmin, pmax, psum, pavg, ppseudo_avg};
+    float data5elements[] = {63.4, 86.65, 25.66, 75.31, 96.65};
+    float data10elements[] = {44.12, 96.63, 75.61, 25.36, 75.12, 63.4, 86.65, 25.66, 75.31, 96.65};
+    printf("\t\tOutput of an array with 5 elements\n");
+    for(int i=0; i<5; i++){
+        printf("Function %d: %7f\n",i+1,(*aggregates[i])(data5elements, 5));
+    }
+    printf("\t\tOutput of an array with 10 elements\n");
+    for(int i=0; i<5; i++){
+       printf("Function %d: %7f\n",i+1, (*aggregates[i])(data10elements, 5));
+    }
 
-   int numberToTest, remainder;
-   printf("Enter the number you want to test: ");
-   scanf("%d", &numberToTest);
-   remainder = numberToTest%2;
-   if(remainder == 0){
-    printf("Number You entered is even: %d\n", numberToTest);
-   }
-   else{
-    printf("Number you entered is odd: %d\n", numberToTest);
-   }
+    printf("\n\n----------------------------------------------------------\n");
+    printf("\t\tOutput for the Question 5 \n");
+    printf("----------------------------------------------------------\n");
+    typedef float (*funcptr[5])(float data[], int size);
 
-   enum weekdays{Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
-   enum weekdays today = Sunday;
-   switch(today){
-    case Monday:
-        printf("Today is Monday.\n");
-        break;
-    case Tuesday:
-        printf("Today is Tuesday.\n");
-        break;
-    case Wednesday:
-        printf("Today is Wednesday.\n");
-        break;
-    case Thursday:
-        printf("Today is Thursday.\n");
-        break;
-    case Friday:
-        printf("Today is Friday.\n");
-        break;
-    case Saturday:
-        printf("Today is Saturday.\n");
-        break;
-    case Sunday:
-        printf("Today is Sunday.\n");
-        break;
-    default:
-        printf("Nothing entered..!!");
-   }
 
-   return 0;
+    const char * funcnames[] = {"FMIN", "FMAX", "FSUM", "FAVG", "FPSEUDO_AVG"};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
